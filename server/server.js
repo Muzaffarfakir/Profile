68354821402454e4aa7352e1f3692e97ce178cdf
Muzaffarfakir <98309599+Muzaffarfakir@users.noreply.github.com>
2023-09-30 14:34:03 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,98 @@
+//////////////////initilize all libs 
+let express = require("express");
+let app = express();
+let cors = require("cors");
+let bodyparser=require("body-parser")
+let path = require("path")
+let port = 8000;
+let jwt = require("jsonwebtoken");
+let mongoose = require("mongoose");
+let multer = require("multer");
+/////////////////////midleawres /////////
+app.use(cors());
+app.use(express.json())
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"upload")))
+/////////////////////setup of multer /////////
+let storage = multer.diskStorage({
+    destination: "upload/",
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+})
+/////////////////upload midleware ////////
+let upload = multer({
+    storage: storage
+})
+let par = {
+    useNewUrlParser: true
+}
+////////////////////////url of addres mongose /////// or connect
+let url = "mongodb://fakirmuzaffar771:Muzaffar@ac-upkty94-shard-00-00.pdzvphx.mongodb.net:27017,ac-upkty94-shard-00-01.pdzvphx.mongodb.net:27017,ac-upkty94-shard-00-02.pdzvphx.mongodb.net:27017/?ssl=true&replicaSet=atlas-3rofmx-shard-0&authSource=admin&retryWrites=true&w=majority"
+mongoose.connect(url, par).then(() => {
+    console.log("eastablished sussecfully")
+}).catch((er) => {
+    console.log(er)
+})
+////////schema ////////////////////////
+let devSc = new mongoose.Schema({
+    name: String,
+    pass: String
+})
+let dataSc = new mongoose.Schema({
+    text: String,
+    title: String,
+    repo : String,
+    img:String,
+    date:Date
+})
+/////////////create A Model///////////
+let dev = new mongoose.model("devSc", devSc);
+let d = new mongoose.model("datasc", dataSc);
+
+////////routings here
+////////////Img /////////////////////
+let img;
+app.post("/projectDataImg", upload.single('imgg'), (req, res) => {
+   img=req.file.filename
+   
+})
+app.post("/projectData",  (req, res) => {
+    console.log(req.body);
+    let {text,title,repo}=req.body;
+    let url=req.protocol+"://"+req.get("host");
+    let data=new d({
+        text:text,
+        title:title,
+        repo:repo,
+        img:url+"/"+img,
+        date:Date.now()
+        
+
+    });
+    data.save();
+
+});
+app.get('/',async(req,res)=>{
+    let data=await d.find({});
+    res.send(data)
+
+})
+
+
+//////////////Developer Login Data ///////
+app.post("/devLogData", async (req, res) => {
+    let { name, pass } = req.body
+    let data = await dev.findOne({ name: name, pass: pass })
+    if (data) {
+        let token = jwt.sign({ id: data._id }, "muju");
+        res.json({ mess: "exist", token: token });
+
+    }
+    else {
+        res.json({ mess: "not" })
+    }
+})
+
+
+app.listen(port);
