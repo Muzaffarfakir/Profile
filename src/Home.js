8d765f405e8dec2b561612a363c12da0a1f8ea29
Muzaffarfakir <98309599+Muzaffarfakir@users.noreply.github.com>
2023-10-01 10:40:17 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,18 @@ import nodejs from "./Nodejs.png"
 import Fotter from "./Fotter";
 import Particle from "./Particle";
 import Navbar from "./Navbar";
+import {useNavigate} from "react-router-dom"
 
 function Home() {
     let s = useRef();
     let p = useRef();
+    let nav=useNavigate();
+    function ab(){
+        nav("/About");
+    }
+       function con(){
+        nav("/Contact");
+    }
 
 
 
@@ -62,8 +70,8 @@ function Home() {
                     <h6>Hello My Name is  Muzaffar This Is My Protofolio Website This is  Made in Reactjs Nodejs Expressjs Mongodb Particles.js </h6>
                     <h6>Here You Get To Know Me Or To Contact  me </h6>
                     <div className="mx-3 mb-3 my-3">
-                        <a className="btn btn-outline-primary mx-3 bg-dark text-white  my-3" href="/About">About Me</a>
-                        <a className="btn btn-outline-primary my-3 bg-dark text-white" href="/Contact">Contact</a>
+                        <a className="btn btn-outline-primary mx-3 bg-dark text-white  my-3" onClick={ab}>About Me</a>
+                        <a className="btn btn-outline-primary my-3 bg-dark text-white" onClick={con}>Contact</a>
 
                     </div>
                     <div className="card  text-center bg-dark text-white ">
